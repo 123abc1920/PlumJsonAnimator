f68ec491058855a923973f88fccdbbd0f4cb9159
123abc1920 <123abc1920@mail.ru>
2025-10-14 16:11:09 +0300

    first commit

---
+++ SpinejsonEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SpinejsonEditor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ SpinejsonEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SpinejsonEditor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
